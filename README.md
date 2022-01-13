4317fc1c0acc2921a1ab69bae99e70f4ebc823c1
Robert Kelly <robert@liatrio.com>
2022-01-13 10:17:58 -0600

    Adding initial action files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # terraform-change-pr-commenter
 GitHub Action to read changes from Terraform plan JSON, summarize changes, and post them in a GitHub Pull Request Comment
+
+## Inputs
+
+### `tfplan-json-file`
+
+**Required** The location of the JSON file created by running `terraform show -no-color -json tfplan.plan > tfplan.json` (Or whatever you choose to name your plan or json outputs)
+
+## Example usage
+
+```yaml
+uses: liatrio/terraform-change-pr-commenter@main
+with:
+  tfplan-show-json: 'tfplan.json'
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+const tfplanJsonFile = core.getInput('tfplan-json-file');
+
+const trackedChanges = {
+    "delete": "-",
+    "create": "+",
+    "update": "!"
+}
+
+const changes = JSON.parse(fs.readFileSync(tfplanJsonFile)).resource_changes;
+
+try {
+    let message = "";
+
+    for (const action in trackedChanges){
+    if(changes.filter(obj => obj.change.actions.includes(action)).length === 0){
+        continue
+    }
+    message += '#### Resources to ${action}: \n'
+    for (const change of changes.filter(obj => obj.change.actions.includes(action))){
+        message += `${trackedChanges[change.change.actions[0]]} ${change.address}\n`
+    }
+    message += '\n'
+    }
+
+    const summary = 'Plan: ' +
+    changes.filter(obj => obj.change.actions[0] === "create").length + ' to add, ' +
+    changes.filter(obj => obj.change.actions[0] === "update").length + ' to change, ' + 
+    changes.filter(obj => obj.change.actions[0] === "delete").length + ' to destroy.'
+
+    const output = `
+    #### Terraform Plan 
+    <details><summary>${summary}</summary>
+
+    \`\`\`diff
+    ${message}
+    \`\`\`
+    </details>`;
+
+    github.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: output
+    })
+} catch (error) {
+    core.setFailed(error.message);
+}
